66ee288c3d150d65cb4fd2b68cb65781d98b4a21
Sharmishtha Nath <73122555+sharmishtha2401@users.noreply.github.com>
2021-07-08 09:37:38 +0530

    Update perfect-number.cpp

--- perfect-number/perfect-number.cpp
+++ perfect-number/perfect-number.cpp
@@ -18,3 +18,10 @@ public:
         
     }
 };
+/*
+bool checkPerfectNumber(int num) {
+        int sum=1;
+        for(int i=2;i<=sqrt(num);i++) if(num%i==0) sum += i + (i==num/i ? 0 : num/i);
+        return sum==num && num!=1;
+    }
+*/
