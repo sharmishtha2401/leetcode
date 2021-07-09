e59233aaea874e829297ec27e0b7f83564089d76
Sharmishtha Nath <73122555+sharmishtha2401@users.noreply.github.com>
2021-07-09 11:31:36 +0530

    Update middle-of-the-linked-list.cpp

--- middle-of-the-linked-list/middle-of-the-linked-list.cpp
+++ middle-of-the-linked-list/middle-of-the-linked-list.cpp
@@ -1,13 +1,3 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
 public:
     ListNode* middleNode(ListNode* head) {
@@ -21,13 +11,10 @@ public:
             cur=cur->next;
         }
         count/=2;
-        
         int len=0;
         cur=head;
-        while(cur)
+        while(len!=count)
         {
-            if(len==count)
-                break;
             cur=cur->next;
             len++;
         }
