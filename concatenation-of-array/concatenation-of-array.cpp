bb79a56e8aa0682b3635c438d48382a0b6de1317
Sharmishtha Nath <73122555+sharmishtha2401@users.noreply.github.com>
2021-07-13 13:12:51 +0530

    Update concatenation-of-array.cpp

--- concatenation-of-array/concatenation-of-array.cpp
+++ concatenation-of-array/concatenation-of-array.cpp
@@ -12,3 +12,15 @@ public:
         
     }
 };
+
+/*
+class Solution {
+public:
+    vector<int> getConcatenation(vector<int>& nums) {
+        int n = nums.size();
+        for(int i = 0; i < n; i++) {
+            nums.push_back(nums[i]);
+        }
+        return nums;
+    }
+}; */
