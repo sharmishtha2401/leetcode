7ca2a7a3436b6cad8b96f1a226defe4dd02265a3
Sharmishtha Nath <73122555+sharmishtha2401@users.noreply.github.com>
2021-08-04 19:06:42 +0530

    Update product-of-array-except-self.cpp

--- product-of-array-except-self/product-of-array-except-self.cpp
+++ product-of-array-except-self/product-of-array-except-self.cpp
@@ -3,7 +3,7 @@ public:
     vector<int> productExceptSelf(vector<int>& nums) {
         int prod=1;
         int is_zero=0;
-        int flag=0;
+    
         for(int i=0; i<nums.size();  i++)
         {
             if(nums[i]==0)
@@ -13,8 +13,7 @@ public:
             }
             prod*=nums[i];
         }
-        if(is_zero!=0)
-            flag=1;
+        
         
         vector<int> product;
         
