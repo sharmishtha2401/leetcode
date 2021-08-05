a88be0cd125292a08a2a47e6dc4155be23d167e8
Sharmishtha Nath <73122555+sharmishtha2401@users.noreply.github.com>
2021-08-05 14:27:38 +0530

    Time: 4 ms (79.41%), Space: 7.5 MB (71.09%) - LeetHub

--- stone-game/stone-game.cpp
+++ stone-game/stone-game.cpp
@@ -2,5 +2,11 @@ class Solution {
 public:
     bool stoneGame(vector<int>& piles) {
         return true;
+        /* Since Alex will always win,
+        he can pick either all the odd pile ors even piles, 
+        and since botch won't be same, 
+        Alex can always win.
+        if the total number of piles wasn't even,
+        it would have been different. */
     }
 };
