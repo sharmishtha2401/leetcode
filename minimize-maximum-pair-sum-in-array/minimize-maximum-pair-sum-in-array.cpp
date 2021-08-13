aa11d707fe392532542941dfe6f61331e01a78d3
Sharmishtha Nath <73122555+sharmishtha2401@users.noreply.github.com>
2021-08-13 11:29:35 +0530

    Update minimize-maximum-pair-sum-in-array.cpp

--- minimize-maximum-pair-sum-in-array/minimize-maximum-pair-sum-in-array.cpp
+++ minimize-maximum-pair-sum-in-array/minimize-maximum-pair-sum-in-array.cpp
@@ -1,6 +1,24 @@
 class Solution {
 public:
     int minPairSum(vector<int>& nums) {
+        sort(nums.begin(), nums.end());      
+        int max=0;
+        for(int i=0; i<nums.size()/2; i++)
+        {
+            int k=nums[i]+nums[nums.size()-i-1];
+            if(k>max)
+                max=k;
+        }
+        return max;
+        
+    }
+};
+
+OR
+
+class Solution {
+public:
+    int minPairSum(vector<int>& nums) {
         vector<int> pair;
         sort(nums.begin(), nums.end());
         int n=nums.size()-1;
