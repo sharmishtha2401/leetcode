2bd4a52be6ad30ac2ccb6e524c1545b5fcec654a
Sharmishtha Nath <73122555+sharmishtha2401@users.noreply.github.com>
2021-08-20 16:28:28 +0530

    Update contains-duplicate.cpp

--- contains-duplicate/contains-duplicate.cpp
+++ contains-duplicate/contains-duplicate.cpp
@@ -10,3 +10,15 @@ public:
         return false;
     }
 };
+
+OR
+
+#include <set>
+using namespace std;
+
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        return nums.size() > set<int>(nums.begin(), nums.end()).size();        
+    }
+}
