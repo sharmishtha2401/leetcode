7595279c2d1d44df6a4714298bf7c43060a03bb8
Sharmishtha Nath <73122555+sharmishtha2401@users.noreply.github.com>
2021-08-20 17:43:37 +0530

    Update largest-perimeter-triangle.cpp

--- largest-perimeter-triangle/largest-perimeter-triangle.cpp
+++ largest-perimeter-triangle/largest-perimeter-triangle.cpp
@@ -5,7 +5,7 @@ public:
         sort(nums.begin(), nums.end(), greater<int>());
         for(int i=0; i<nums.size()-2; i++)
         {
-            if(nums[i]+nums[i+1]>nums[i+2]  && nums[i]+nums[i+2]>nums[i+1] && nums[i+2]+nums[i+1]>nums[i])
+            if(nums[i]<nums[i+1]+nums[i+2])
                 return nums[i]+nums[i+1]+nums[i+2];
         }
         return 0;
