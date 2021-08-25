497f09191bf94f59002bd8114aee7459b2cfe1c0
Sharmishtha Nath <73122555+sharmishtha2401@users.noreply.github.com>
2021-08-25 18:04:32 +0530

    Update find-greatest-common-divisor-of-array.cpp

--- find-greatest-common-divisor-of-array/find-greatest-common-divisor-of-array.cpp
+++ find-greatest-common-divisor-of-array/find-greatest-common-divisor-of-array.cpp
@@ -4,14 +4,14 @@ public:
         int mn, mx;
         mn=*min_element(nums.begin(), nums.end());
         mx=*max_element(nums.begin(), nums.end());
-        int d=gcd(mn, mx);
-        return d;
+        return gcd(mn,mx);
+        
     }
-    
+    /*
     int gcd(int a, int b)
     {
         if(a%b==0)
             return b;
         return gcd(b, a%b);
-    }
+    } */
 };
