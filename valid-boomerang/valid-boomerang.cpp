315a7c1112043b24c9968efc5c79e58572ac2077
Sharmishtha Nath <73122555+sharmishtha2401@users.noreply.github.com>
2021-08-28 15:41:19 +0530

    Update valid-boomerang.cpp

--- valid-boomerang/valid-boomerang.cpp
+++ valid-boomerang/valid-boomerang.cpp
@@ -4,3 +4,9 @@ public:
         return((0.5*(p[0][0]*(p[1][1]-p[2][1]) + p[1][0]*(p[2][1]-p[0][1]) + p[2][0]*(p[0][1]-p[1][1])))!=0);
     }
 };
+
+/*
+Formula for area of triangle is : 
+0.5 * [x1 * (y2 - y3) + x2 * (y3 - y1) + x3 * (y1 - y2)]
+ if area of triangle is 0,  three points are collinear
+ */
