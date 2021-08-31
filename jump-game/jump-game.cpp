1f44e4959252f0fd9d1b96dae84e0112546a6951
Sharmishtha Nath <73122555+sharmishtha2401@users.noreply.github.com>
2021-08-31 15:30:21 +0530

    Update jump-game.cpp

--- jump-game/jump-game.cpp
+++ jump-game/jump-game.cpp
@@ -2,8 +2,10 @@ class Solution {
 public:
     bool canJump(vector<int>& nums) {
          int reach=0,i;
-        for(i=0; i<nums.size() && i<=reach; i++)
+        for(i=0; i<nums.size(); i++)
         {
+            if(i>reach)
+                return false;
             reach=max(i+nums[i], reach);
         }
         return i==nums.size();
