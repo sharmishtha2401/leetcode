4e45213b7206c1f71729de935fbe4d38b63bca5e
Sharmishtha Nath <73122555+sharmishtha2401@users.noreply.github.com>
2021-09-08 19:31:06 +0530

    Time: 20 ms (84.12%), Space: 31 MB (67.40%) - LeetHub

---
+++ find-pivot-index/find-pivot-index.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int pivotIndex(vector<int>& nums) {
+        if(nums.size()==1)
+            return 0;
+        
+        int sum=0;
+        for(int i=0; i<nums.size(); i++)
+        {
+            sum+=nums[i];
+        }
+            
+        int first=0;
+        int second=0;
+        for(int i=0; i<nums.size(); i++)
+        {
+            
+            first+=i==0?0:nums[i-1];
+            second=sum-first-nums[i];
+            if(first==second)
+                return i;
+        }
+        return -1;
+    }
+};
