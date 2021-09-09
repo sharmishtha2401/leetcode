5a794e84ad4b2bb9403efb2c7c038828d92144c6
Sharmishtha Nath <73122555+sharmishtha2401@users.noreply.github.com>
2021-09-09 17:17:54 +0530

    Update three-divisors.cpp

--- three-divisors/three-divisors.cpp
+++ three-divisors/three-divisors.cpp
@@ -14,3 +14,33 @@ public:
         return count==3;
     }
 };
+
+OR
+
+/*
+
+If you observe this closely, the numbers having exact 3 divisors are those which are a perfect square of a prime number.
+So the task is to find :
+i. If n is a perfect square
+ii. If the square root of n is a prime number  
+
+
+//Approach-2 (Slightly improved)
+class Solution {
+public:
+    bool isThree(int n) {
+        int div = 2; //1 and n are already divisors of n
+        
+        for(auto d = 2; d <= sqrt(n) && div < 3; d++) {
+            if(n%d == 0) {
+                div++;
+                if(d*d != n) div++;
+            }
+            
+        }
+        
+        return div==3;
+    }
+};
+
+*/
