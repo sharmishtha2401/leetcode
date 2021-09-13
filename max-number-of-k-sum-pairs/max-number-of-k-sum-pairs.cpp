3a2ae526bb8ca1a88cd521b996a16994cf556217
Sharmishtha Nath <73122555+sharmishtha2401@users.noreply.github.com>
2021-09-13 15:13:07 +0530

    Update max-number-of-k-sum-pairs.cpp

--- max-number-of-k-sum-pairs/max-number-of-k-sum-pairs.cpp
+++ max-number-of-k-sum-pairs/max-number-of-k-sum-pairs.cpp
@@ -1,3 +1,4 @@
+//USING HASHMAP/Unordered map, O(N)
 class Solution {
 public:
     int maxOperations(vector<int>& nums, int k) {
@@ -15,6 +16,30 @@ public:
             {
                 hash[nums[i]]++;
             }
+        }
+        return count;
+    }
+};
+
+//Using Two pointer, O(N.logN), sort the array first
+class Solution {
+public:
+    int maxOperations(vector<int>& nums, int k) {
+        sort(nums.begin(), nums.end());
+        int count=0;
+        int i=0, j=nums.size()-1;
+        while(i<j)
+        {
+            if(nums[i]+nums[j]==k)
+            {
+                count++; 
+                i++; 
+                j--;
+            }
+            else if(nums[i]+nums[j]>k)
+                j--;
+            else
+                i++;
         }
         return count;
     }
