1d62d2151d255edaff7ff5f1f8f4f8f28460895d
Sharmishtha Nath <73122555+sharmishtha2401@users.noreply.github.com>
2021-09-15 19:41:17 +0530

    Time: 4 ms (92.09%), Space: 6 MB (87.66%) - LeetHub

--- roman-to-integer/roman-to-integer.cpp
+++ roman-to-integer/roman-to-integer.cpp
@@ -1,44 +1,42 @@
 class Solution {
 public:
     int romanToInt(string s) {
-        int num=0;
+        int n=0;
         int len=s.size();
         for(int i=0; i<len; i++)
         {
             if(i<len-1 && roman(s[i])<roman(s[i+1]))
             {
-                num-=roman(s[i]);
-            }
+                n-=roman(s[i]);
+            }         
             else
             {
-                num+=roman(s[i]);
-            }
+                n+=roman(s[i]);
+            }    
         }
-        return num;
+        return n;
     }
     
     int roman(char a)
-    {
-        switch(a)
         {
-            case 'I':
-                return 1;
-            case 'V':
-                return 5;
-            case 'X':
-                return 10;
-            case 'L':
-                return 50;
-            case 'C':
-                return 100;
-            case 'D':
-                return 500;
-            case 'M':
-                return 1000;
-            default:
-                return 0;
-                
-        }
-    }
-    
+            switch(a)
+            {
+                case 'I':
+                    return 1;
+                case 'V':
+                    return 5;
+                case 'X':
+                    return 10;
+                case 'L':
+                    return 50;
+                case 'C':
+                    return 100;
+                case 'D':
+                    return 500;
+                case 'M':
+                    return 1000;
+                default:
+                    return 0;
+            }
+        } 
 };
