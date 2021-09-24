ff77b0ec98bd89277735ca5c2e0d49f42944594c
Sharmishtha Nath <73122555+sharmishtha2401@users.noreply.github.com>
2021-09-24 13:38:03 +0530

    Update n-th-tribonacci-number.cpp

--- n-th-tribonacci-number/n-th-tribonacci-number.cpp
+++ n-th-tribonacci-number/n-th-tribonacci-number.cpp
@@ -19,3 +19,21 @@ public:
         return d;
     }
 };
+
+OR
+
+class Solution {
+public:
+    int tribonacci(int n) {
+        vector<long long int> T(38,0);
+        T[0]=0;
+        T[1]=1;
+        T[2]=1;
+        for(int i=3; i<=n; i++)
+        {
+            T[i]=T[i-1]+T[i-2]+T[i-3];
+        }
+        return T[n];
+        
+    }
+};
