51855bb2c1f3aa219312c938343e1c6ec849d3c6
Sharmishtha Nath <73122555+sharmishtha2401@users.noreply.github.com>
2021-09-29 09:33:11 +0530

    Time: 4 ms (97.56%), Space: 7.3 MB (44.23%) - LeetHub

--- valid-anagram/valid-anagram.cpp
+++ valid-anagram/valid-anagram.cpp
@@ -2,19 +2,19 @@ class Solution {
 public:
     bool isAnagram(string s, string t) {
         vector<int> alpha(26, 0);
-        vector<int> beta(26, 0);
+        //vector<int> beta(26, 0);
         for(int i=0; i<s.size(); i++)
         {
             alpha[s[i]-'a']++;
         }
         for(int i=0; i<t.size(); i++)
         {
-            beta[t[i]-'a']++;
+            alpha[t[i]-'a']--;
             
         }
         for(int i=0; i<26; i++)
         {
-            if(alpha[i]!=beta[i])
+            if(alpha[i]!=0)
                 return false;
         }
         return true;
