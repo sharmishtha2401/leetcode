f8863b3a2ff5262c4f9151fdead31eeb83877bde
Sharmishtha Nath <73122555+sharmishtha2401@users.noreply.github.com>
2021-10-05 10:15:59 +0530

    Time: 6 ms (9.58%), Space: 5.8 MB (97.48%) - LeetHub

--- climbing-stairs/climbing-stairs.cpp
+++ climbing-stairs/climbing-stairs.cpp
@@ -1,3 +1,12 @@
+/*  Basically it's a fibbonacci sereis:
+    step : ways
+     1      1  {1}
+     2      2  {1,1},{2}
+     3      3  {1,1,1},{1,2},{2,1}
+     4      5  {1,1,1,1},{1,1,2},{1,2,1},{2,1,1},{2,2}
+          
+ */
+
 class Solution {
 public:
     int climbStairs(int n) {
