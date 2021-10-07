798fe02da49e5dfd4c1eb849702ca54412e9e6ef
Sharmishtha Nath <73122555+sharmishtha2401@users.noreply.github.com>
2021-10-07 20:27:40 +0530

    Update flipping-an-image.cpp

--- flipping-an-image/flipping-an-image.cpp
+++ flipping-an-image/flipping-an-image.cpp
@@ -1,13 +1,10 @@
 class Solution {
 public:
     vector<vector<int>> flipAndInvertImage(vector<vector<int>>& image) {
-        for(int i=0; i<image.size(); i++)
-        {
-            reverse(image[i].begin(), image[i].end());
-        }
         
         for(int i=0; i<image.size(); i++)
         {
+            reverse(image[i].begin(), image[i].end());
             for(int j=0; j<image[i].size(); j++)
             {
                 if(image[i][j]==0)
