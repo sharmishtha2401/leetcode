8e725f598c7e82209814074b2bd21046dd114bbb
Sharmishtha Nath <73122555+sharmishtha2401@users.noreply.github.com>
2021-10-08 14:16:03 +0530

    Update rotate-string.cpp

--- rotate-string/rotate-string.cpp
+++ rotate-string/rotate-string.cpp
@@ -1,8 +1,39 @@
 class Solution {
 public:
     bool rotateString(string s, string goal) {
+        if(s.size()!=goal.size())
+            return false;
+        queue<int> q1, q2;
+        for(auto c : s)
+        {
+            q1.push(c);
+        }
+        for(auto c : goal)
+        {
+            q2.push(c);
+        }
+        int i=0; 
+        int n=s.size();
+        while(i<n)
+        {
+            if(q1==q2)
+                return true;
+            
+            q2.push(q2.front());
+            q2.pop();
+            i++;
+        }
+        return false;
+    }
+};
+
+/*
+class Solution {
+public:
+    bool rotateString(string s, string goal) {
         if(s.size()==goal.size() && (s+s).find(goal)!=string::npos)
             return true;
         return false;
     }
 };
+*/
